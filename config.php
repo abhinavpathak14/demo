e38c7105b2486cc2068402f25315e325ccbeffc3
abhinav.pathak.14@gmail.com <abhinav.pathak.14@gmail.com>
2014-11-22 11:52:21 +0530

    config update

--- config.php
+++ config.php
@@ -1,17 +1,14 @@
 <?php
 session_start();
 $dbopts = parse_url(getenv('DATABASE_URL'));
-echo '<pre>';
-print_r($dbopts);
-die;
-$app->register(new Herrera\Pdo\PdoServiceProvider(),
+/*$app->register(new Herrera\Pdo\PdoServiceProvider(),
   array(
     'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
     'pdo.port' => $dbopts["port"],
     'pdo.username' => $dbopts["user"],
     'pdo.password' => $dbopts["pass"]
   )
-);
+);*/
 $config['base_url'] = "https://pure-ridge-6265.herokuapp.com/";
 //database setting
 define('HOST', 'localhost');
@@ -19,7 +16,7 @@ define('USERNAME', 'root');
 define('PASSWORD', '');
 define('DB_NAME', 'demo');
 define('PORT', '3306');
-
+$con = pg_connect('host='.$dbopts["host"].' port='.$dbopts["port"].' dbname=hobby-dev user='.$dbopts["user"].' password='.$dbopts["pass"]) or die('not connected');
 $con = mysqli_connect(HOST, USERNAME, PASSWORD, DB_NAME, PORT) or die('Unable to connect with database');
 
 require_once 'functions.php';
