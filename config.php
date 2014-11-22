c10c797c6ed85230c613cc811f48533c6b0cb677
abhinav.pathak.14@gmail.com <abhinav.pathak.14@gmail.com>
2014-11-22 16:44:49 +0530

    update config

--- config.php
+++ config.php
@@ -9,16 +9,16 @@ $dbopts = parse_url(getenv('DATABASE_URL'));
     'pdo.password' => $dbopts["pass"]
   )
 );*/
-$config['base_url'] = "http://localhost/demo/"; //https://pure-ridge-6265.herokuapp.com/
+$config['base_url'] = "https://pure-ridge-6265.herokuapp.com/"; //https://pure-ridge-6265.herokuapp.com/
 //database setting
 define('HOST', 'localhost');
 define('USERNAME', 'root');
 define('PASSWORD', '');
 define('DB_NAME', 'demo');
 define('PORT', '3306');
-$con = pg_connect("host=localhost port=5432 dbname=demo user=postgres password=admin ");
+//$con = pg_connect("host=localhost port=5432 dbname=demo user=postgres password=admin ");
 
-//$con = pg_connect('host=ec2-54-204-37-113.compute-1.amazonaws.com port=5432 dbname=d7pfnmq3bve4mf user=xomfzdcsikgdhn password=LwYduG8aST_alIsV67ICVXWgfg') or die('not connected');
+$con = pg_connect('host=ec2-54-204-37-113.compute-1.amazonaws.com port=5432 dbname=d7pfnmq3bve4mf user=xomfzdcsikgdhn password=LwYduG8aST_alIsV67ICVXWgfg') or die('not connected');
 //$con = mysqli_connect(HOST, USERNAME, PASSWORD, DB_NAME, PORT) or die('Unable to connect with database');
 
 require_once 'functions.php';
