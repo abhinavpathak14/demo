027d13e0dfa73e2cc67ec313b7954454e12b9fee
abhinav.pathak.14@gmail.com <abhinav.pathak.14@gmail.com>
2014-11-22 11:34:05 +0530

    database updated

--- demo.sql
+++ demo.sql
@@ -1,51 +1,49 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.27, for Win32 (x86)
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 21, 2014 at 10:21 PM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+-- Host: localhost    Database: demo
+-- ------------------------------------------------------
+-- Server version	5.5.27
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO,POSTGRESQL' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `demo`
+-- Table structure for table "users"
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS "users";
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE "users" (
+  "user_id" int(11) NOT NULL,
+  "email" varchar(200) NOT NULL,
+  "name" varchar(200) NOT NULL,
+  "user_password" varchar(200) NOT NULL,
+  "city" varchar(200) NOT NULL,
+  "created_by" int(11) DEFAULT NULL,
+  "created_on" date NOT NULL,
+  "IsAdmin" int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY ("user_id")
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `users`
+-- Dumping data for table "users"
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(200) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `user_password` varchar(200) NOT NULL,
-  `city` varchar(200) NOT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_on` date NOT NULL,
-  `IsAdmin` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+LOCK TABLES "users" WRITE;
+/*!40000 ALTER TABLE "users" DISABLE KEYS */;
+INSERT INTO "users" VALUES (2,'test@fdg.com','Abhinav','e10adc3949ba59abbe56e057f20f883e','Gurgaon',NULL,'0000-00-00',0),(3,'admin@gmail.com','Admin','e10adc3949ba59abbe56e057f20f883e','Gurgaon',NULL,'0000-00-00',1);
+/*!40000 ALTER TABLE "users" ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Dumping data for table `users`
---
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-INSERT INTO `users` (`user_id`, `email`, `name`, `user_password`, `city`, `created_by`, `created_on`, `IsAdmin`) VALUES
-(2, 'test@fdg.com', 'Abhinav', 'e10adc3949ba59abbe56e057f20f883e', 'Gurgaon', NULL, '0000-00-00', 0),
-(3, 'admin@gmail.com', 'Admin', 'e10adc3949ba59abbe56e057f20f883e', 'Gurgaon', NULL, '0000-00-00', 1);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Dump completed on 2014-11-22 11:27:14
